af11247d1d75a5f08a5331663006c0fa00fc315f
Wagão - ( Wagner Goulart ) <wagner.goulart@neon.com.br>
2022-07-15 22:41:00 -0300

    feat: layout of login screen created

--- Twitter/Controller/Authentication/LoginController.swift
+++ Twitter/Controller/Authentication/LoginController.swift
@@ -21,27 +21,41 @@ class LoginController: UIViewController {
     
     private lazy var emailContainerView: UIView = {
         let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
-        let view = Utilities().inputContainerView(withImage: image)
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
         return view
     }()
     
     private lazy var passwordContainerView: UIView = {
         let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
-        let view = Utilities().inputContainerView(withImage: image)
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
         return view
     }()
     
+    private lazy var loginButton: UIView = {
+        let button = Utilities().loginButton(withTitle: "Log In")
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var dontHaveAccountButton: UIButton = {
+        let button = Utilities().attributedButton("Don't have an account?", " Sign Up")
+        button.addTarget(self, action: #selector(handleCreateAccount), for: .touchUpInside)
+        return button
+    }()
+    
     private lazy var emailTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Email"
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        tf.autocapitalizationType = .words
         return tf
     }()
     
     private lazy var passwordTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Password"
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
         return tf
     }()
+    
+   
     
     //MARK: - Lifecycle
     
@@ -53,6 +67,14 @@ class LoginController: UIViewController {
     
     //MARK: - Selectors
     
+    @objc func handleLogin(){
+        print("DEBUG: Deveria ta fazendo o login...")
+    }
+    
+    @objc func handleCreateAccount(){
+        print("DEBUG: Navigate to another screen...")
+    }
+    
     //MARK: - Helpers
     
     func configureUI() {
@@ -63,11 +85,17 @@ class LoginController: UIViewController {
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor )
         logoImageView.setDimensions(width: 150, height: 150)
         
-        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
         stack.axis = .vertical
-        stack.spacing = 8
+        stack.spacing = 20
+        stack.distribution = .fillEqually
         
         view.addSubview(stack)
-        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                     right: view.rightAnchor, paddingLeft: 40, paddingBottom: 16,
+                                     paddingRight: 40)
     }
 }
--- Twitter/Utils/Utilities.swift
+++ Twitter/Utils/Utilities.swift
@@ -12,7 +12,7 @@ class Utilities {
     func inputContainerView(withImage image: UIImage, textField: UITextField) -> UIView {
         let view = UIView()
         view.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        view.backgroundColor = .red
+        view.tintColor = .white
         let iv = UIImageView()
         iv.image = image
         view.addSubview(iv)
@@ -20,8 +20,51 @@ class Utilities {
         iv.setDimensions(width: 24, height: 24)
         
         view.addSubview(textField)
-        textField.anchor(left: iv.leftAnchor, bottom: <#T##NSLayoutYAxisAnchor?#>)
+        textField.anchor(left: iv.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        let dividerView = UIView()
+        dividerView.backgroundColor = .white
+        view.addSubview(dividerView)
+        dividerView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingRight: 4, height: 0.50)
         
         return view
+    }
+    
+    func textField(withPlaceholder placeholder: String) -> UITextField {
+        let tf = UITextField()
+        tf.placeholder = placeholder
+        tf.textColor = .white
+        tf.autocapitalizationType = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        return tf
+    }
+    
+    func loginButton(withTitle title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(lessThanOrEqualToConstant: 50).isActive = true
+        button.layer.cornerRadius = 25 / 3
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return button
+    }
+    
+    func attributedButton(_ fristPart: String, _ secondPart: String) -> UIButton {
+        let button = UIButton(type: .system)
+        
+        let attributedTitle = NSMutableAttributedString(string: fristPart, attributes: [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor: UIColor.white])
+
+        attributedTitle.append(NSMutableAttributedString(string: secondPart, attributes: [
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor: UIColor.white]))
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+
+        return button
     }
 }
