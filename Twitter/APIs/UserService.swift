0bc112b663b4a75321dc5655dd9a77a2c3d6ef35
Wagão - ( Wagner Goulart ) <wagner.goulart@neon.com.br>
2022-07-18 17:22:19 -0300

    refact: refactor the dictionary way

--- Twitter/APIs/UserService.swift
+++ Twitter/APIs/UserService.swift
@@ -13,8 +13,8 @@ struct UserService {
     func fetchUser() {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
-        REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshoot in
-            guard let dictionary = snapshoot.value as? [String: AnyObject] else { return }
+        REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
             let user = User(uid: uid, dictionary: dictionary)
             
             print("DEBUG: Username is \(user.username)")
